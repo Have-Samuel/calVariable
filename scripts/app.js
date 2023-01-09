0530724af9cb7cc341c3c92d013b69947fea27d0
Have Samuel <samiehave9@gmail.com>
2023-01-09 23:43:42 +0300

    Avoiding Repeat yourself

--- scripts/app.js
+++ scripts/app.js
@@ -2,8 +2,9 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 function add() {
-  const calcDescription =`${currentResult} + ${userInput.value}`
-  currentResult += parseInt(userInput.value);
+  const enteredNumber = parseInt(userInput.value)
+  const calcDescription =`${currentResult} + ${enteredNumber}`
+  currentResult += enteredNumber;
   outputResult(currentResult, calcDescription);
 }
 
