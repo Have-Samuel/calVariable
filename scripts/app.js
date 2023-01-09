8a984078b7f616d3382a82240ced7ca3cbcba84f
Have Samuel <samiehave9@gmail.com>
2023-01-10 00:12:50 +0300

    Connecting buttons to events

--- scripts/app.js
+++ scripts/app.js
@@ -5,11 +5,40 @@ function getUserNumberInput() {
   return parseInt(userInput.value);
 }
 
-function add() {
-  const enteredNumber = getUserNumberInput();
-  const calcDescription =`${currentResult} + ${enteredNumber}`
-  currentResult += enteredNumber;
+function createAndWriteOutput(operator, resultBeforeCal, calNumber) {
+  const calcDescription = `${resultBeforeCal} ${operator} ${calNumber}`;
   outputResult(currentResult, calcDescription);
 }
 
+function add() {
+  const initialResult = currentResult;
+  const enteredNumber = getUserNumberInput();
+  currentResult += enteredNumber;
+  createAndWriteOutput('+', initialResult, enteredNumber)
+}
+
+function subtract() {
+  const initialResult = currentResult;
+  const enteredNumber = getUserNumberInput();
+  currentResult -= enteredNumber;
+  createAndWriteOutput('-', initialResult, enteredNumber)
+}
+
+function multiply() {
+  const initialResult = currentResult;
+  const enteredNumber = getUserNumberInput();
+  currentResult *= enteredNumber;
+  createAndWriteOutput('*', initialResult, enteredNumber)
+}
+
+function divide() {
+  const initialResult = currentResult;
+  const enteredNumber = getUserNumberInput();
+  currentResult /= enteredNumber;
+  createAndWriteOutput('/', initialResult, enteredNumber)
+}
+
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
